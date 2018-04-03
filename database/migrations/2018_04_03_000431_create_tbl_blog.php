8e4d4b0ed403f142fb06b90644c4e6e16f1a378c
Eyakub <eyakubsorkar@gmail.com>
2018-04-03 06:06:39 +0600

    solve the migration problem

--- database/migrations/2018_04_03_000431_create_tbl_blog.php
+++ database/migrations/2018_04_03_000431_create_tbl_blog.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateTb1Table extends Migration
+class CreateTblBlog extends Migration
 {
     /**
      * Run the migrations.
